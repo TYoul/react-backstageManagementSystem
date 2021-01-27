3f4e4efdc5ba6b4ce5174ec051769e561fcb761f
chenzhouxu <710916361@qq.com>
2021-01-27 15:39:13 +0800

    侧边栏优化

--- src/components/left-menu/LeftMenu.tsx
+++ src/components/left-menu/LeftMenu.tsx
@@ -1,37 +1,40 @@
-import React from 'react';
-import './LeftMenu.scss';
-import { Menu } from 'antd';
-import { AppstoreOutlined, PieChartOutlined, DesktopOutlined, ContainerOutlined, MailOutlined } from '@ant-design/icons';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "./LeftMenu.scss";
+import menuList from "./menu-config";
+import { Menu } from "antd";
 
-const { SubMenu } = Menu;
+const { SubMenu, Item } = Menu;
 
 const LeftMenu: React.FC = () => {
+  const history = useHistory();
+
+  const handleReplace = (path: string) => {
+    history.replace(path);
+  };
+
+  const getMenuItem = (menuList: any) => {
+    return menuList.map((menu: any) =>
+      menu.children ? (
+        <SubMenu key={menu.key} icon={menu.icon} title={menu.title}>
+          {getMenuItem(menu.children)}
+        </SubMenu>
+      ) : (
+        <Item
+          key={menu.key}
+          icon={menu.icon}
+          onClick={(e) => handleReplace(menu.path)}
+        >
+          {menu.title}
+        </Item>
+      )
+    );
+  };
+
   return (
     <div className="sider-container">
-      <Menu defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} mode="inline" theme="dark">
-        <Menu.Item key="1" icon={<PieChartOutlined />}>
-          Option 1
-        </Menu.Item>
-        <Menu.Item key="2" icon={<DesktopOutlined />}>
-          Option 2
-        </Menu.Item>
-        <Menu.Item key="3" icon={<ContainerOutlined />}>
-          Option 3
-        </Menu.Item>
-        <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
-          <Menu.Item key="5">Option 5</Menu.Item>
-          <Menu.Item key="6">Option 6</Menu.Item>
-          <Menu.Item key="7">Option 7</Menu.Item>
-          <Menu.Item key="8">Option 8</Menu.Item>
-        </SubMenu>
-        <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
-          <Menu.Item key="9">Option 9</Menu.Item>
-          <Menu.Item key="10">Option 10</Menu.Item>
-          <SubMenu key="sub3" title="Submenu">
-            <Menu.Item key="11">Option 11</Menu.Item>
-            <Menu.Item key="12">Option 12</Menu.Item>
-          </SubMenu>
-        </SubMenu>
+      <Menu defaultSelectedKeys={["home"]} mode="inline" theme="dark">
+        {getMenuItem(menuList)}
       </Menu>
     </div>
   );
---
+++ src/components/left-menu/menu-config.js
@@ -0,0 +1,83 @@
+import {
+  HomeOutlined,
+  AppstoreOutlined,
+  BarsOutlined,
+  ToolOutlined,
+  UserOutlined,
+  TeamOutlined,
+  AreaChartOutlined,
+  BarChartOutlined,
+  LineChartOutlined,
+  PieChartOutlined,
+} from "@ant-design/icons";
+
+// 项目的菜单配置
+const menuList = [
+  {
+    title: "首页", // 菜单标题名称
+    key: "home", // 对应的path
+    icon: <HomeOutlined />, // 图标名称
+    path: "/home", //对应路径
+  },
+  {
+    title: "商品",
+    key: "prod_about",
+    icon: <AppstoreOutlined />,
+    children: [
+      // 子菜单列表
+      {
+        title: "分类管理",
+        key: "category",
+        icon: <BarsOutlined />,
+        path: "/prod/category",
+      },
+      {
+        title: "商品管理",
+        key: "product",
+        icon: <ToolOutlined />,
+        path: "/prod/product",
+      },
+    ],
+  },
+
+  {
+    title: "用户管理",
+    key: "user",
+    icon: <UserOutlined />,
+    path: "/user",
+  },
+  {
+    title: "角色管理",
+    key: "role",
+    icon: <TeamOutlined />,
+    path: "/role",
+  },
+
+  {
+    title: "图形图表",
+    key: "charts",
+    icon: <AreaChartOutlined />,
+    children: [
+      {
+        title: "柱形图",
+        key: "bar",
+        icon: <BarChartOutlined />,
+        path: "/charts/bar",
+      },
+      {
+        title: "折线图",
+        key: "line",
+        icon: <LineChartOutlined />,
+        path: "/charts/line",
+      },
+      {
+        title: "饼图",
+        key: "pie",
+        icon: <PieChartOutlined />,
+        path: "/charts/pie",
+      },
+    ],
+  },
+];
+
+export default menuList;
