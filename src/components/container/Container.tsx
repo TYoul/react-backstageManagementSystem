275bfdd48a66c8d19f0860970b333462ad7b39ac
chenzhouxu <710916361@qq.com>
2021-01-30 22:15:59 +0800

    初步添加表单

--- src/components/container/Container.tsx
+++ src/components/container/Container.tsx
@@ -20,9 +20,6 @@ import BarPage from "../../pages/bar/Bar";
 import LinePage from "../../pages/line/Line";
 import PiePage from "../../pages/pie/Pie";
 
-// 测试
-// import { getCategory } from '../../services/category';
-
 const ContainerPage: React.FC = () => {
   // const dispatch = useDispatch();
   const login = useSelector((state) => state.login, shallowEqual);
@@ -56,10 +53,7 @@ const ContainerPage: React.FC = () => {
             <Redirect to="/home" />
           </Switch>
         </Content>
-        <Footer className="footer">
-          React后台管理系统@TYoul
-          {/* <button onClick={e => getCategory()}>点我</button> */}
-        </Footer>
+        <Footer className="footer">React后台管理系统@zhou_xu</Footer>
       </Layout>
     </Layout>
   );
--- src/pages/category/Category.tsx
+++ src/pages/category/Category.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, shallowEqual } from "react-redux";
 import { useSelector } from "../../redux/hooks";
-import { Button, Card, Table } from "antd";
+import { Button, Card, Table, Modal, Form, Input } from "antd";
 import { ColumnsType } from "antd/es/table";
 import { PlusOutlined } from "@ant-design/icons";
 
@@ -17,11 +17,37 @@ const CategoryPage: React.FC = () => {
     shallowEqual
   );
 
-  // 在redux中进行网络请求
+  // 弹窗是否显示的变量:isModalVisible
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [modalTitle, setModalTitle] = useState("");
+  const [form] = Form.useForm();
+
+  // 在redux中进行网络请求，获取商品分类列表
   useEffect(() => {
     dispatch(getCategoryListAction());
   }, [dispatch]);
 
+  // 弹窗显示
+  const showModal = (key: any) => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    if (modalTitle === "修改分类") {
+      console.log("你要修改");
+    } else {
+      console.log("你要添加");
+    }
+    form.resetFields();
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    form.resetFields();
+    setIsModalVisible(false);
+  };
+
+  // table columns数据
   const columns: ColumnsType = [
     {
       key: "name",
@@ -29,14 +55,22 @@ const CategoryPage: React.FC = () => {
       dataIndex: "name",
     },
     {
-      key: "operating",
+      key: "operation",
       title: "操作",
-      dataIndex: "key",
-      render: (key) => {
+      dataIndex: "name",
+      render: (name) => {
         return (
-          <Button type="link" onClick={() => console.log(key)}>
-            修改分类
-          </Button>
+          <>
+            <Button
+              type="link"
+              onClick={(e) => {
+                setModalTitle("修改分类");
+                showModal(name);
+              }}
+            >
+              修改分类
+            </Button>
+          </>
         );
       },
       width: "15%",
@@ -44,25 +78,57 @@ const CategoryPage: React.FC = () => {
     },
   ];
 
+  // table 数据
   const data: any[] = categoryList;
 
   return (
-    <Card
-      extra={
-        <Button type="primary">
-          <PlusOutlined />
-          添加
-        </Button>
-      }
-    >
-      <Table
-        columns={columns}
-        dataSource={data}
-        bordered
-        rowKey="_id"
-        pagination={{ pageSize: PAGE_SIZE }}
-      />
-    </Card>
+    <>
+      <Card
+        extra={
+          <Button
+            type="primary"
+            onClick={(e) => {
+              setModalTitle("添加分类");
+              showModal(e);
+            }}
+          >
+            <PlusOutlined />
+            添加
+          </Button>
+        }
+      >
+        <Table
+          columns={columns}
+          dataSource={data}
+          bordered
+          rowKey="_id"
+          pagination={{ pageSize: PAGE_SIZE }}
+          scroll={{ y: "calc(100vh - 557px)" }}
+        />
+      </Card>
+      <Modal
+        title={modalTitle}
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        okText="确认"
+        cancelText="取消"
+      >
+        <Form form={form}>
+          <Form.Item
+            name="operation"
+            rules={[
+              {
+                required: true,
+                message: "分类名称必须输入",
+              },
+            ]}
+          >
+            <Input placeholder="分类名必须输入" />
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
   );
 };
 
